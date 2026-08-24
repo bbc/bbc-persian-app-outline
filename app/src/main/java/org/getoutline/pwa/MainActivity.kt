2a0c634e549349e6a6f3224788d15b433c166ae3
abdallahalsalmi <abdallah.al-salmi01@bbc.co.uk>
2026-08-24 12:47:42 +0100

    first commit for tv - [co-pilot]

--- app/src/main/java/org/getoutline/pwa/MainActivity.kt
+++ app/src/main/java/org/getoutline/pwa/MainActivity.kt
@@ -1,6 +1,8 @@
 package org.getoutline.pwa
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.webkit.ProxyConfig
 import androidx.webkit.ProxyController
 import androidx.webkit.WebViewFeature
@@ -15,11 +17,28 @@ class MainActivity : BridgeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        )
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        if (this.bridge.webView != null) {
+            this.bridge.webView.isFocusable = true
+            this.bridge.webView.isFocusableInTouchMode = true
+            this.bridge.webView.requestFocus()
+        }
+
         if (WebViewFeature.isFeatureSupported(WebViewFeature.PROXY_OVERRIDE)) {
             this.proxy = Mobileproxy.runProxy(
                 "127.0.0.1:0",
                 Mobileproxy.newSmartStreamDialer(
-                    Mobileproxy.newListFromLines("www.bbc.com"),
+                    Mobileproxy.newListFromLines("www.bbc.com/persian/watch/bbc_persian_tv/live"),
                     "{\"dns\":[{\"https\":{\"name\":\"9.9.9.9\"}}],\"tls\":[\"\",\"split:1\",\"split:2\",\"tlsfrag:1\"]}",
                     Mobileproxy.newStderrLogWriter()
                 )
